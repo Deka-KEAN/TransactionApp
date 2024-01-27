ce0859192fb7854aa9105bd4851b1a5d5fa7d7a4
Deka-KEAN <anupdeka698@gmail.com>
2024-01-28 04:37:39 +0530

    Updating Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,15 +10,19 @@ import { Send } from "./pages/Send";
 function App() {
   const [authenticated,setAuthentication]=useState(false);
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/signup" element={<SignUp/>}/>
-          <Route path="/signin" element={<SignIn/>}/>
-          <Route path="/dashboard" element={<Dashboard/>}/>
-          <Route path="/send" element={<Send/>}/>
-        </Routes>
-      </Router>
+    <div className="w-screen">
+      <h1 className="left-0 top-0 p-2 font-extrabold">TxnPay</h1>
+      <div className="h-fit flex items-center justify-center">
+        
+        <Router>
+          <Routes>
+            <Route path="/signup" element={<SignUp/>}/>
+            <Route path="/signin" element={<SignIn/>}/>
+            <Route path="/dashboard" element={<Dashboard/>}/>
+            <Route path="/send" element={<Send/>}/>
+          </Routes>
+        </Router>
+      </div>
     </div>
   )
 }
--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -4,8 +4,30 @@
 
 export function SignIn(){
     return (
-        <div>
-            signin
+        <div className="h-fit p-4 w-[400px]">
+            <h1 className="text-center p-2 font-bold">Sign Up</h1>
+            <p className="p-2 text-xl text-center opacity-40 font-medium">Enter your information to create an account</p>
+            <form className="max-w-sm mx-auto">
+                <div className="mb-3">
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
+                    <input type="text" placeholder="John" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"></input>
+                </div>
+                <div className="mb-3">
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
+                    <input type="text" placeholder="Doe" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"></input>
+                </div>
+                <div className="mb-3">
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                    <input type="text" placeholder="johndoe@example.com" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"></input>
+                </div>
+                <div className="mb-3">
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                    <input type="password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"></input>
+                </div>
+                <div className="text-center p-2">
+                    <button type="submit" class="text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register new account</button>
+                </div>
+            </form>
         </div>
     );
 }
